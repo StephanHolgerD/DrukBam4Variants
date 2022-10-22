cbe6254ab528e460dd4ba90bd505575ce6c2440a
StephanHolgerD <stephdruk@googlemail.com>
2022-10-22 14:05:07 +0200

    Datacreator and Vcflooker classes

--- LookingForVCF.py
+++ LookingForVCF.py
@@ -1,42 +1,39 @@
+from symbol import comp_if
 import pysam
 import numpy as np
 import pandas as pd
 import sys
-
-
+from DataCollector import DataCollector_cls
+from VariantLooker import VariantLooker_cls
 
 
 
 
 class ReadFiles():
-    def __init__(self,ReferenceFasta,BamFile,VcfFile) -> None:
+    def __init__(self,ReferenceFasta,BamFile,VcfFile):
         self.ReferenceFasta=ReferenceFasta
         self.BamFile = BamFile
         self.VcfFile = VcfFile
+        self.DataCollector = DataCollector_cls(self.ReferenceFasta,self.BamFile)
         a = self.IterVariants()
-        print(a)
 
 
     def IterVariants(self):
         with pysam.VariantFile(self.VcfFile) as f:
             for variant in f:
-                print(variant)
-                ReadData = self.GetReads(variant.contig,variant.pos)
-                print(ReadData)
-
-
-    def GetReads(self,chrom,pos):
-        ResultArray = []
-        with pysam.AlignmentFile(self.BamFile) as f:
-            for alignment in f.fetch(chrom,pos):
-                ResultArray.append(alignment.reference_start)
-        return ResultArray
+                c=0
+                AlignmentData = self.DataCollector.GetReads(str(variant.contig),variant.pos)
+                for alignment in AlignmentData:
+                    x = VariantLooker_cls(variant,alignment)
+                    x.EvaluateAlignment()
     
 
+
+            
 
 if __name__ == "__main__":
     
     b = sys.argv[1]
     v = sys.argv[2]
-    
-    xx = ReadFiles('foo',b,v)
+    f = sys.argv[3]
+    xx = ReadFiles(f,b,v)
