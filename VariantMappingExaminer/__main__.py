7162cb7d9008f8c77bc1921cc2ce06b9b1c3905d
StephanHolgerD <stephdruk@googlemail.com>
2022-11-06 22:10:51 +0100

    tsv out

--- VariantMappingExaminer/__main__.py
+++ VariantMappingExaminer/__main__.py
@@ -1,6 +1,7 @@
 from multiprocessing.managers import BaseManager
 from VariantMappingExaminer.ReadFiles.VcfReader import ReadVcfFile
 from VariantMappingExaminer.WriteFiles.VcfWriter import WriteVcf_cls
+from VariantMappingExaminer.WriteFiles.TsvWriter import WriteTSV_cls
 from VariantMappingExaminer.ParseAlignments.VariantCounter import VariantCounter_cls
 from time import time
 import argparse
@@ -11,13 +12,30 @@ def main():
     subparsers = parser.add_subparsers(help='write results to info field of input vcf')
 
     vcf_parser = subparsers.add_parser("vcf")
+    tsv_parser = subparsers.add_parser("tsv")
+    
 
     requiredNamed = vcf_parser.add_argument_group('required arguments')
     requiredNamed.add_argument('-b','--bam', help='Pos. sorted and indexed bam file', required=True)
     requiredNamed.add_argument('-v','--vcf', help='vcf file with variants of interest', required=True)
+    requiredNamed.add_argument('-o','--vcfout', help='vcf output', required=True)
+    
+    requiredNamed.add_argument('-f','--fasta',help='fasta file of reference',required=True)
+    
+
+    requiredNamed = tsv_parser.add_argument_group('required arguments')
+    requiredNamed.add_argument('-b','--bam', help='Pos. sorted and indexed bam file', required=True)
+    requiredNamed.add_argument('-v','--vcf', help='vcf file with variants of interest', required=True)
+    requiredNamed.add_argument('-o','--tsvout', help='vcf output', required=True)
+    
     requiredNamed.add_argument('-f','--fasta',help='fasta file of reference',required=True)
 
     optArguments = vcf_parser.add_argument_group('optional arguments')
+    optArguments.add_argument('--cpu',default=1, help="number of cpu's  to run in paralell",type=int)
+    
+    
+    
+    optArguments = tsv_parser.add_argument_group('optional arguments')
     optArguments.add_argument('--cpu',default=1, help="number of cpu's  to run in paralell",type=int)
 
     if len(sys.argv)==1:
@@ -26,7 +44,20 @@ def main():
 
     args=parser.parse_args()
 
-    if args.__contains__('vcf'):
+    
+    print(dir(args))
+    if args.__contains__('tsvout'):
+        t=time()
+        fasta = args.fasta
+        bam = args.bam
+        vcf = args.vcf
+        proc = args.cpu
+        x = ReadVcfFile(fasta,bam,vcf)
+        VC_object = VariantCounter_cls(x,processes=proc)
+        Vcf_Writer_object = WriteTSV_cls(VC_object,x)
+        print(time()-t)
+
+    if args.__contains__('vcfout'):
         t=time()
         fasta = args.fasta
         bam = args.bam
@@ -36,7 +67,6 @@ def main():
         VC_object = VariantCounter_cls(x,processes=proc)
         Vcf_Writer_object = WriteVcf_cls(VC_object,x)
         print(time()-t)
-
 if __name__ == "__main__":
     main()
 
